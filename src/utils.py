7d02cea36e45dc70118ae5f989de1d93517b9c08
shahidsak1973 <shaikshahidvali200@gmail.com>
2024-03-10 15:34:50 +0530

    training

---
+++ src/utils.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pickle
+import numpy as np 
+import pandas as pd
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models,x,y):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            test_model_score=cross_val_score(model, x, y, cv=10)
+
+            report[list(models.keys())[i]] =  test_model_score.mean()
+
+        return report
+
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
+
+
+    
